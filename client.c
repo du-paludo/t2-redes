025628c2586b21acd938a949fb9f8c7c4606abda
Eduardo Paludo <eduardo.paludo8@gmail.com>
2023-05-24 16:24:49 -0300

    return commented

--- client.c
+++ client.c
@@ -115,7 +115,7 @@ int main(int argc, char **argv) {
             if (recvfrom(socket_desc, received_message, sizeof(received_message), 0,
                 (struct sockaddr*)&previous_addr, &previous_struct_length) < 0) {
                 printf("Couldn't receive message\n");
-                return -1;
+                // return -1;
             }
             
             // printf("Received message: %s\n", received_message);
